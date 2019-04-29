d97c6d1c2a8a787c704b3e088f83cedb05647eba
sean-one <basement365@gmail.com>
2019-04-28 22:35:04 -0700

    added clientRouter and added to server

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// routes
+const clientRouter = require('./api/routes/clientRoutes');
+
 app.use(express.json());
 app.use(cors());
+
+// use routes
+app.use('/api/clients', clientRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json({ api: 'alive!' });
