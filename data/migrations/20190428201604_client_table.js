7c68b4232f5c5760181229f7b567118dfb54f45d
sean-one <basement365@gmail.com>
2019-04-28 20:27:36 -0700

    added client_table migration file

---
+++ data/migrations/20190428201604_client_table.js
@@ -0,0 +1,36 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('clients', clients => {
+        clients.increments('id');
+
+        clients
+            .string('firstname', 20)
+            .notNullable();
+        
+        clients
+            .string('lastname', 30)
+            .notNullable();
+        
+        clients
+            .string('email', 128)
+            .notNullable()
+            .unique();
+        
+        clients
+            .integer('phone', 10);
+        
+        clients
+            .string('address', 256)
+        
+        clients
+            .string('city', 256)
+        
+        clients
+            .string('state', 2)
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+  
+};
