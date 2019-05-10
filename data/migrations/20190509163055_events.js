8ce88ab30fb9460c10ce73b7f99fcbeedf54bb35
SeanOne <basement365@gmail.com>
2019-05-09 21:45:06 -0700

    added events migration

---
+++ data/migrations/20190509163055_events.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('events', events => {
+        events.increment('id');
+
+        events
+            .string('event_name')
+            .notNullable();
+        
+        events
+            .date('event_startDate')
+            .notNullable();
+        
+        events
+            .date('event_endDate')
+            .notNullable();
+        
+        events
+            .string('event_details')
+            .notNullable();
+        
+        events
+            .string('event_link');
+
+        events
+            .string('event_image')
+            .notNullable();
+        
+    })
+  
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('events');
+};
