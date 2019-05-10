cace12b08d09e885cab9358234299d4e50b9182e
SeanOne <basement365@gmail.com>
2019-05-09 22:00:22 -0700

    added get all events routes

--- api/routes/eventRoutes.js
+++ api/routes/eventRoutes.js
@@ -4,3 +4,11 @@ const db = require('../../data/eventsModel.js');
 
 const router = express.Router();
 
+//GET all events | sanity check
+router.get('/', (req, res) => {
+    db.find()
+        .then(event => {
+            res.status(200).json(events);
+        })
+        .catch(err => res.status(500).json(err));
+});
