73dbd4ccc8fc3748f04763ac5e97f82c5b17dd61
SeanOne <basement365@gmail.com>
2019-05-10 22:57:59 -0700

    added event by id endpoint

--- api/routes/eventRoutes.js
+++ api/routes/eventRoutes.js
@@ -13,4 +13,23 @@ router.get('/', (req, res) => {
         .catch(err => res.status(500).json(err));
 });
 
+// GET event by event id
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(event => {
+            if (event) {
+                res.status(200).json(event);
+            } else {
+                res.status(404).json({ message: 'no event found' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'this is a server error', err });
+        });
+});
+
+
+
 module.exports = router;
